81ff812507b4f3a8968c5b9d8bdaaeef15d01c63
AntLukeLiu <antmliu@gmail.com>
2016-10-25 15:55:14 -0400

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a = 63;
+		int b = 45;
+		a = b;
+		System.out.println("a is "+ a);
+		System.out.println("b is "+ b);
+
+		double c = 3.14159;
+
+	}
+
+}
